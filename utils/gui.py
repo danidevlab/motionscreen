fbed5d0fcef44ea3af560f5a74b69fd65c74e034
Dani <daniell0626@naver.com>
2026-04-16 15:30:34 +0900

    브라우저 표시

--- utils/gui.py
+++ utils/gui.py
@@ -1,1 +1,12 @@
-import tkinter
+import tkinter as tk
+from tkinterweb import HtmlFrame
+
+root = tk.Tk()
+root.title("내장 브라우저")
+
+frame = HtmlFrame(root)
+frame.pack(fill="both", expand=True)
+
+frame.load_website("http://127.0.0.1:5000")
+
+root.mainloop()
